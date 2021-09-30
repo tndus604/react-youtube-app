1c1b5479c7985f8a884930052875cff0efcba2f2
tndus604 <tndus0604@hotmail.com>
2021-09-30 17:23:25 -0400

    upload page layout created

--- client/src/components/views/UploadVideoPage/UploadVideoPage.js
+++ client/src/components/views/UploadVideoPage/UploadVideoPage.js
@@ -7,11 +7,27 @@ import axios from 'axios';
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const privacy = [
+    { value: 0, label:'Private' },
+    { value: 1, label:'Public' }
+]
+
+const Category = [
+    { value: 0, label: "Gaming" },
+    { value: 0, label: "Beauty & Fashion" },
+    { value: 0, label: "Music" },
+    { value: 0, label: "Comedy" },
+    { value: 0, label: "Kids" },
+    { value: 0, label: "Sports" },
+    { value: 0, label: "Tech" },
+    { value: 0, label: "Cooking & Health" }
+]
+
 function UploadVideoPage() {
     const [title, setTitle] = useState("");
     const [Description, setDescription] = useState("");
-    const [privacy, setPrivacy] = useState(0)
-    const [Categories, setCategories] = useState("Film & Animination");
+    const [Privacy, setPrivacy] = useState(0)
+    const [Categories, setCategories] = useState("Gaming");
     const [FilePath, setFilePath] = useState("")
 
     const handleChangeTitle = (event) => {
@@ -21,15 +37,50 @@ function UploadVideoPage() {
     const handleChangeDescription = (event) => {
         setDescription(event.currentTarget.value);
     }
+    
+    const handleChangeOne = (event) => {
+        setPrivacy(event.currentTarget.value)
+    }
+
+    const handleChangeTwo = (event) => {
+        setCategories(event.currentTarget.value)
+    }
+
+    const onSubmit = () => {
+
+    }
+
+    const onDrop = (files) => {
+        let formData = new FormData();
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        console.log(files);
+        formData.append("files", files[0])
+
+        axios.post('/api/video/uploadfiles', formData, config)
+        .then(response => {
+            if(response.data.success) {
+                let variable = {
+                    filePath: response.data.filePath,
+                    filenAME: response.data.fileName
+                }
+                setFilePath(response.data.filePath)
+            } else {
+                alert('failed to save the video in server')
+            }
+        })
+    }
 
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}>Upload Video</Title>
             </div>
-            <Form>
+            <Form onSubmit={onSubmit}>
+                <div style={{display: 'flex', justifyContent:'space-between'}}>
                 <Dropzone 
-                    
+                    onDrop={onDrop}
                     multiple={false}
                     maxSize={800000000}>
                     {({ getRootProps, getInputProps }) => (
@@ -42,28 +93,40 @@ function UploadVideoPage() {
                         </div>
                     )}
                 </Dropzone>
+                </div>
+                <br/>
                 <label>Title</label>
                 <Input 
                     onChange={handleChangeTitle}
                     value={title}
                 />
+                <br/>
                 <label>Description</label>
                 <TextArea 
                     onChange={handleChangeDescription}
                     value={Description}
                 />
-
-                <Button type="primary" size="large">
+                <select onChange={handleChangeOne}>
+                    {privacy.map((item, index) => (
+                        <option key={index} value={item.value}>
+                            {item.label}
+                        </option>
+                    ))}
+                </select>
+                <br/>
+                <select onChange={handleChangeTwo}>
+                    {Category.map((item, index) => (
+                        <option key={index} value={item.label}>
+                            {item.label}
+                        </option>
+                    ))}
+                </select>
+                <br/>
+                <Button type="primary" size="large" onClick=    {onSubmit}>
                     Upload
                 </Button>
+
             </Form>
-            {/* <Form onSubmit={onSubmit}>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Dropzone onDrop={onDrop}
-                    multiple={false}
-                    maxSize={80000000000}
-                </div>
-            </Form> */}
         </div>
     )
 }
